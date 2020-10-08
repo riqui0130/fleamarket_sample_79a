f93eb97d54bfe9e7075271f6ee7ea6b3cbabaad9
riqui0130 <riquiffen8ru@icloud.com>
2020-10-09 05:33:25 +0900

    a

--- db/schema.rb
+++ db/schema.rb
@@ -9,3 +9,15 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_10_05_155633) do
+
+  create_table "sells", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "text"
+    t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
