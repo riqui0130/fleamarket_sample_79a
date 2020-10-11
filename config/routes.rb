b94df1f206077d381886bfe7619ea19390b0b4c1
sf2020-LATTE <second.s.fujii@gmail.com>
2020-10-11 15:10:37 +0900

    create-view-of-purchase

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'items#index'
+  resources :items do
+    collection do
+      get 'buy'
+    end
+  end
 end
