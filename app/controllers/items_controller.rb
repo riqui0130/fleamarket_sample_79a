4e37c21997ea7d8266247c34a05fadd2e0c8186b
riqui0130 <riquiffen8ru@icloud.com>
2020-10-11 18:09:15 +0900

    a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,31 @@ class ItemsController < ApplicationController
   end
 
   def new
+    # @item = Item.new
+    # @category_parent =  Category.where("ancestry is null")
+  end
+
+  def category_children
+    @category_children = Category.find("#{params[:parent_id]}").children
+  end
+
+  def category_grandchildren
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :text, :category_id, :condition_id, :deliverycost_id, :pref_id, :delivery_days_id, :price, images: []).merge(user_id: current_user.id, boughtflg_id:"1")
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'items#index'
-  resources :items, only: [:show, :new]
+  resources :items, only: [:show, :new, :create, :update] do
+    collection do
+      get 'get_category_child', to: 'items#get_category_child', defaults: { format: 'json' }
+      get 'get_category_grandchild', to: 'items#get_category_grandchild', defaults: { format: 'json' }
+    end
+  end
 end
