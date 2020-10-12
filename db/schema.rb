603175d2838ad6475bc84f5c87bbfd7736802d2d
riqui0130 <riquiffen8ru@icloud.com>
2020-10-13 04:00:12 +0900

    a

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_021112) do
+ActiveRecord::Schema.define(version: 2020_10_12_183643) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.text "image", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_images_on_item_id"
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 2020_10_12_021112) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "images", "items"
 end
