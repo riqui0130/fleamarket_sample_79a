71966d26f9c7473b401f3b51b48df41dc269ef78
riqui0130 <riquiffen8ru@icloud.com>
2020-10-13 03:45:23 +0900

    a

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :condition
-  belongs_to_active_hash :pref
+  belongs_to_active_hash :prefecture
   belongs_to_active_hash :deliverycost
   belongs_to_active_hash :delivery_days
 
--- db/migrate/20201012021112_create_items.rb
+++ db/migrate/20201012021112_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name, null: false
+      t.text :detail, null: false
+      t.integer :price, null: false
+      t.integer :condition_id, null: false
+      t.integer :delivery_days_id, null: false
+      t.integer :deliverycost_id, null: false
+      t.integer :prefecture_id, null: false
+      t.string :brand
+      t.references :category, null: false
       t.timestamps
     end
   end
