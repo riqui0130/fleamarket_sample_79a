a46c87606adecfcaaa393c0c33f01f950717460b
riqui0130 <riquiffen8ru@icloud.com>
2020-10-12 11:44:49 +0900

    item

---
+++ app/models/item.rb
@@ -0,0 +1,28 @@
+class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :pref
+  belongs_to_active_hash :deliverycost
+  belongs_to_active_hash :delivery_days
+
+  validate :images_presence
+  validates :name, :text, :category_id, :condition_id, :deliverycost_id, :pref_id, :delivery_days_id,:user_id, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+  has_many_attached :images
+  belongs_to :user, foreign_key: 'user_id'
+  belongs_to :category
+
+  #imageのバリデーション
+  def images_presence
+    if images.attached?
+      # inputに保持されているimagesがあるかを確認
+      if images.length > 10
+        errors.add(:image, '10枚まで投稿できます')
+      end
+    else
+      errors.add(:image, '画像がありません')
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,3 +9,20 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_10_12_021112) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "sells", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "text"
+    t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
