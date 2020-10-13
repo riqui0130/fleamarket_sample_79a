95660a180407a738996fc3270adaa02d2e70f829
riqui0130 <riquiffen8ru@icloud.com>
2020-10-14 08:43:43 +0900

    a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,14 +8,18 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     # @item.images.new
+    @category_parent_array = []
+    # @category = Category.roots.each do |parent|
+    # @category_parent_array << parent.name
+    # end
   end
 
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to root_path, notice: "出品しました"
     else
-      render :new
+      render :new, alert: "出品できません。入力必須項目を確認してください"
     end
   end
 
