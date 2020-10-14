e169a809f673f42e3bc20325ede47b4a2bc0f84d
riqui0130 <riquiffen8ru@icloud.com>
2020-10-14 21:59:03 +0900

    a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,11 +7,16 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
-    # @item.images.new
-    @category_parent_array = []
-    # @category = Category.roots.each do |parent|
-    # @category_parent_array << parent.name
-    # end
+    # @item.item_images.new
+    @category_parent_array = ['選択して下さい']
+    @category_parent_array = Category.where(ancestry: nil)
+  end
+
+  def get_category_children
+    @category_children = Category.find(params[:parent_id]).children
+  end
+  def get_category_grandchildren
+    @category_grandchildren = Category.find(params[:child_id]).children
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'items#index'
   get 'items/buy'
-  resources :items, only: [:show, :new]
-    resources :items do
-      collection do
-        get 'buy'
-      end
+  resources :items do
+    collection do
+      get 'category/get_category_children', to: 'items#get_category_children', defaults: { format: 'json' }
+      get 'category/get_category_grandchildren', to: 'items#get_category_grandchildren', defaults: { format: 'json' }
     end
+  end
 end
