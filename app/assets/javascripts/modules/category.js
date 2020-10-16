0f4993bb3a2be8a5d09eef67f8a1968e144c339f
riqui0130 <riquiffen8ru@icloud.com>
2020-10-17 08:07:28 +0900

    a

--- app/assets/javascripts/modules/category.js
+++ app/assets/javascripts/modules/category.js
@@ -5,7 +5,7 @@ $(document).on('turbolinks:load', function(){
   }
   // 親カテゴリー選択後のイベント
   $('#parent_category').on('change', function(){
-    let parentCategoryId = $(this).val();
+    var parentCategoryId = $(this).val();
     //選択された親カテゴリーのIDを取得
     if (parentCategoryId == ''){
       //親カテゴリーが空（初期値）の時
@@ -23,14 +23,14 @@ $(document).on('turbolinks:load', function(){
         $('#select-children-box').remove();
         $('#select-grandchildren-box').remove();
         //親が変更された時、子と孫を削除するする
-        let optionHtml = '';
+        var optionHtml = '';
         category_children.forEach(function(child){
           optionHtml += categoryOption(child);
           //option要素を作成する
         });
-        $('#error-category').before(`<div class="sell-collection_select " id="select-children-box">
-                                        <label class="sell-collection_select__label" for="item_category_id">
-                                          <select class="sell-collection_select__input" id="category-select-children" required="required" name="item[category_id]">
+        $('#error-category').before(`<div class="sell-collection_select__category " id="select-children-box">
+                                        <label class="sell-collection_select__category__label" for="item_category_id">
+                                          <select class="sell-collection_select__category__input" id="category-select-children" required="required" name="item[category_id]">
                                             <option value="">選択して下さい</option>
                                             ${optionHtml}
                                           </select>
@@ -46,7 +46,7 @@ $(document).on('turbolinks:load', function(){
   });
   // 子カテゴリー選択後のイベント
   $('.sell-container__content__details').on('change', '#category-select-children', function(){
-    let childrenCategoryId = $(this).val();
+    var childrenCategoryId = $(this).val();
     //選択された子カテゴリーのIDを取得
     if (childrenCategoryId == ''){
       //子カテゴリーが空（初期値）の時
@@ -62,14 +62,14 @@ $(document).on('turbolinks:load', function(){
       .done(function(category_grandchildren){
         $('#select-grandchildren-box').remove();
         //子が変更された時、孫を削除するする
-        let optionHtml = '';
+        var optionHtml = '';
         category_grandchildren.forEach(function(grandchildren){
           optionHtml += categoryOption(grandchildren);
           //option要素を作成する
         });
-        $('#error-category').before(`<div class="sell-collection_select " id="select-grandchildren-box">
-                                        <label class="sell-collection_select__label" for="item_category_id">
-                                          <select class="sell-collection_select__input" id="category-select-grandchildren" required="required" name="item[category_id]">
+        $('#error-category').before(`<div class="sell-collection_select__category " id="select-grandchildren-box">
+                                        <label class="sell-collection_select__category__label" for="item_category_id">
+                                          <select class="sell-collection_select__category__input" id="category-select-grandchildren" required="required" name="item[category_id]">
                                             <option value="">選択して下さい</option>
                                             ${optionHtml}
                                           </select>
