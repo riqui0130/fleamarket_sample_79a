7a8e95eb636b8e8a993e72fb923f0fb51235f4e7
riqui0130 <riquiffen8ru@icloud.com>
2020-11-01 17:31:43 +0900

    table_edit

--- db/migrate/20201028143814_add_cateid_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddCateidToItems < ActiveRecord::Migration[6.0]
-  def change
-    add_column :items, :category_id, :integer, foreign_key:true, null:false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_143814) do
+ActiveRecord::Schema.define(version: 2020_10_24_154725) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "ancestry"
@@ -44,13 +44,24 @@ ActiveRecord::Schema.define(version: 2020_10_28_143814) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "item_name", null: false
-    t.text "description", null: false
     t.integer "price", null: false
+    t.string "name", null: false
+    t.text "detail", null: false
+    t.integer "condition_id", null: false
+    t.integer "delivery_days_id", null: false
+    t.integer "deliverycost_id", null: false
+    t.integer "prefecture_id", null: false
+    t.string "brand"
+    t.bigint "category_id", null: false
+    t.bigint "seller_id"
+    t.bigint "buyer_id"
+    t.bigint "auction_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "buyer_id"
-    t.integer "seller_id", null: false
-    t.integer "category_id", null: false
+    t.index ["auction_id"], name: "index_items_on_auction_id"
+    t.index ["buyer_id"], name: "index_items_on_buyer_id"
+    t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["seller_id"], name: "index_items_on_seller_id"
   end
 
   create_table "pictures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -81,4 +92,7 @@ ActiveRecord::Schema.define(version: 2020_10_28_143814) do
 
   add_foreign_key "destinations", "users"
   add_foreign_key "images", "items"
+  add_foreign_key "items", "users", column: "auction_id"
+  add_foreign_key "items", "users", column: "buyer_id"
+  add_foreign_key "items", "users", column: "seller_id"
 end
