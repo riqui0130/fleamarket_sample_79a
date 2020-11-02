dee0cb4f48c041fe18d3073a76486c47bfbf5fda
riqui0130 <riquiffen8ru@icloud.com>
2020-11-02 22:30:09 +0900

    a

--- db/migrate/20201012183643_create_images.rb
+++ db/migrate/20201012183643_create_images.rb
@@ -1,7 +1,7 @@
 class CreateImages < ActiveRecord::Migration[6.0]
   def change
     create_table :images do |t|
-      t.string :images, null: false
+      t.string :image, null: false
       t.references :item, null:false, foreign_key:true
       t.timestamps
     end
