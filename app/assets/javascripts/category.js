e0fc39669c168e022b215b7ab516ebdcfcdb227d
riqui0130 <riquiffen8ru@icloud.com>
2020-11-02 21:46:46 +0900

    a

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -13,7 +13,7 @@ $(function(){
                           <select>
                         </div>
                       </div>`;
-    $('.sell-collection_select__category__a').append(childSelectHtml);
+    $('.sell-collection_select__category').append(childSelectHtml);
   }
   function appendGrandchidrenBox(insertHTML){
     var grandchildSelectHtml = '';
@@ -25,7 +25,7 @@ $(function(){
                                 </select>
                               </div>
                             </div>`;
-    $('.sell-collection_select__category__a').append(grandchildSelectHtml);
+    $('.sell-collection_select__category').append(grandchildSelectHtml);
   }
 
   $('#parent_category').on('change', function(){
