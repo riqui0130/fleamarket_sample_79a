af5b6e1c67e2c380d7598083bd33713bef83362b
riqui0130 <riquiffen8ru@icloud.com>
2020-11-05 18:09:44 +0900

    s３の変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development? || Rails.env.test?
-    storage :file
+    storage :fog
   else
     storage :fog
   end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,6 @@ CarrierWave.configure do |config|
   if Rails.env.development? || Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
-    config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
@@ -15,8 +14,5 @@ CarrierWave.configure do |config|
     }
     config.fog_directory  = 'fleamarketsample79a'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fleamarketsample79a'
-  else
-    config.storage = :file
-    config.enable_processing = false if Rails.env.test?
   end
 end
