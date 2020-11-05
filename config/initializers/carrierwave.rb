032ccf3ccd1f231d6c42ca751f53758038e30b07
riqui0130 <riquiffen8ru@icloud.com>
2020-11-05 15:46:03 +0900

    本番環境error対応２

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test?
-    config.storage = :file
-  else
+  if Rails.env.development? || Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_public = false
@@ -17,5 +15,8 @@ CarrierWave.configure do |config|
     }
     config.fog_directory  = 'fleamarketsample79a'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fleamarketsample79a'
+  else
+    config.storage = :file
+    config.enable_processing = false if Rails.env.test?
   end
 end
