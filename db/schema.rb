101f1348106af9701f18d8e1953fb109cfc07d31
riqui0130 <riquiffen8ru@icloud.com>
2020-11-23 19:06:07 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_11_20_104816) do
-
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "ancestry"
